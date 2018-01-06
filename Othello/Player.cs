1ec707678f0082b0a2c493ebcde61953f9537708
Thomas Lovis <thomas.lovis@gmail.com>
2018-01-06 14:38:05 +0100

    add player and images

---
+++ Othello/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Othello
+{
+    public class Player
+    {
+        public Player(PawnColor _color)
+        {
+            if ((int)_color > -1)
+            {
+                Color = _color;
+            }
+            else
+            {
+                throw new InvalidDataException($"{_color} error");
+            }
+        }
+
+        public PawnColor Color { get; set; }
+    }
+}
