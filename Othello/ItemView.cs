cba383982823f53a5f8b51ba9c300b931921d2ed
Thomas Lovis <thomas.lovis@gmail.com>
2018-01-10 14:12:15 +0100

    add

---
+++ Othello/ItemView.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Othello
+{
+    public class ItemView
+    {
+        public ItemView(string _imageSource, UserControl _view)
+        {
+            ImageSource = _imageSource;
+            View = _view;
+        }
+
+        public string ImageSource { get; set; }
+
+        public UserControl View { get; set; }
+    }
+}
